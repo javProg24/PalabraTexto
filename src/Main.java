2d7465a2d4c57759d929e99c6520e8730b8aefaf
javProg24 <javiertomalag14@gmail.com>
2025-04-14 14:57:52 -0500

    Quitando Runneable

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,9 @@ import javax.swing.*;
 import vista.frmTablaPalabra;
 public class Main {
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                frmTablaPalabra ventana = new frmTablaPalabra();
-                ventana.setVisible(true);
-            }
+        SwingUtilities.invokeLater(() -> {
+            frmTablaPalabra ventana = new frmTablaPalabra();
+            ventana.setVisible(true);
         });
     }
 }
